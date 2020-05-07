bf8f7be65fa3472b70dec1ecee6bd8a74a726abe
杨科 <yangke@chinafocus.net>
2020-05-07 17:20:12 +0800

    直接从jcenter的私有仓库引入代码

--- permissionsX/src/main/java/com/chinafocus/permissionsx/PermissionXFragment.java
+++ permissionsX/src/main/java/com/chinafocus/permissionsx/PermissionXFragment.java
@@ -10,35 +10,25 @@ import androidx.fragment.app.FragmentActivity;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author
- * @date 2020/5/7
- * description：
- */
+
 public class PermissionXFragment extends Fragment {
 
     private IPermissionListener mListener;
-    //    private String[] mPermissions;
     private final int REQUEST_CODE_PERMISSION = 1;
 
     void requestRuntimePermission(FragmentActivity activity, String[] permissions, IPermissionListener listener) {
-//        mPermissions = permissions;
         mListener = listener;
 
         List<String> permissionList = new ArrayList<>();
 
         for (String permission : permissions) {
-            // 核对权限是否需要授权
             if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 permissionList.add(permission);
             }
         }
         if (!permissionList.isEmpty()) {
-            // 对需要授权的权限，启用申请授权对话框
-            // 这里启用
             this.requestPermissions(permissions, REQUEST_CODE_PERMISSION);
         } else {
-            //doSomeThing
             if (listener != null) {
                 listener.onGranted();
             }
@@ -54,24 +44,20 @@ public class PermissionXFragment extends Fragment {
                 for (int i = 0; i < grantResults.length; i++) {
                     int grantResult = grantResults[i];
                     String permission = permissions[i];
-                    //如果结果有非通过的权限，就加入到集合中做处理
                     if (grantResult != PackageManager.PERMISSION_GRANTED) {
                         deniedPermissions.add(permission);
                     }
                 }
 
                 if (deniedPermissions.isEmpty()) {
-                    //doSomeThing
                     if (mListener != null) {
                         mListener.onGranted();
                     }
                 } else {
-                    //集合不为空，表示有拒绝的权限。启用回调方法
                     if (mListener != null) {
                         mListener.onDenied(deniedPermissions);
                     }
                 }
-
             }
         }
     }
